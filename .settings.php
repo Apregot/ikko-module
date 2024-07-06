bfd6aacbdd7094d904374db199345313da518554
neckita39 <nikita.kantiana@gmail.com>
2024-07-06 10:53:40 +0200

    controller

---
+++ .settings.php
@@ -0,0 +1,12 @@
+<?php
+return [
+	'controllers' => [
+		'value' => [
+			'namespaces' => [
+				'\\Bitrix\\Ikkomodule\\Controller' => 'api'
+			],
+			'defaultNamespace' => '\\Bitrix\\Ikkomodule\\Controller',
+		],
+		'readonly' => true,
+	],
+];
--- lib/Controller/Ikko.php
+++ lib/Controller/Ikko.php
@@ -2,20 +2,53 @@
 
 declare(strict_types=1);
 
-namespace Bitrix\Ikko\Controller;
+namespace Bitrix\Ikkomodule\Controller;
 
 use Bitrix\Ikko\Configuration\Config;
+use Bitrix\Main\Engine\Controller;
+use Bitrix\Main\Engine\Response\AjaxJson;
+use Bitrix\Main\HttpResponse;
 use Bitrix\Main\Web\HttpClient;
 use Bitrix\Main\Web\Json;
 
-class Ikko extends \Bitrix\Main\Engine\Controller
+class Ikko extends Controller
 {
 	protected HttpClient $httpClient;
-	public function postMenuAction()
+	public function postMenuAction(): HttpResponse
 	{
 		$response = $this->httpClient->post(Config::getIkkoUrl());
 		$data = Json::decode($response);
-		$menu = $data['menu'];
+		if ($data['status'] === 'success')
+		{
+			$menu = $data['menu'];
+		}
+
+		return new AjaxJson();
+	}
+
+	public function onItemAddedAction(array $addedItem = [], array $menu = []): HttpResponse
+	{
+		return new AjaxJson();
+	}
+
+	public function onItemRemovedAction(array $removedItem = [], array $menu = []): HttpResponse
+	{
+		return new AjaxJson();
+	}
+
+	public function onShiftPausedAction(): HttpResponse
+	{
+		return new AjaxJson();
+	}
+
+	public function onShiftStartedAction(): HttpResponse
+	{
+		return new AjaxJson();
+	}
+
+	public function onShiftEndedAction(): HttpResponse
+	{
+		return new AjaxJson();
 	}
 
 	protected function init(): void
